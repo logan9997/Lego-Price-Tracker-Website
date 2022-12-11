5d4e095a597e0b7a5c08781588d8a244850b99e4
logan9997 <80635281+logan9997@users.noreply.github.com>
2022-12-11 22:58:58 +0000

    script that will parse through all themes/sub themes and add every one of their sets to a json file

--- my_scripts/item_id_scraper.py
+++ my_scripts/item_id_scraper.py
@@ -9,6 +9,7 @@ class Scraper():
     def __init__(self) -> None:
         self.driver = webdriver.Chrome()
         self.url = "https://www.bricklink.com/catalogTree.asp?itemType=S"
+        self.item_ids = [] #need seperate for minifig and sets
 
 
     def setup(self) -> None:
@@ -51,14 +52,26 @@ class Scraper():
 
 
     def get_theme_ids(self) -> None:
-        
+        with open("theme_details.json", "r") as file:
+            file_content = file.read()
+        #return all a tags with href including corisponding link text
         theme_links = self.driver.find_elements(By.CSS_SELECTOR, 'a[href*="catalogList.asp?catType=S&catString"]')
-        print(len(theme_links))
-        for t in theme_links:
-            print("BEOFRE CLICK")
-            t.click()
+        for theme_path_index, theme in enumerate([theme.get_attribute("href") for theme in theme_links]):
+            if self.paths[theme_path_index] in file_content:
+                print("CONTINUE", self.paths[theme_path_index])
+                continue
+            self.driver.get(theme)
+            time.sleep(1)
 
-            
+            #if there is only one set in a theme, bricklink redirects to catalog item page
+            if "catalogitem.page" in self.driver.current_url:
+                item_id = self.driver.current_url.strip("https://www.bricklink.com/v2/catalog/catalogitem.page?S=")
+                item_id = item_id.split("#")[0]
+                print(item_id)
+                self.item_ids.append(item_id)
+                continue
+
+            #set defualt values for each new theme
             num_items = int(self.driver.find_element(By.XPATH, '//*[@id="id-main-legacy-table"]/tbody/tr/td/table/tbody/tr[3]/td/div/div[2]/div[2]/b[1]').text)
             page_counter = 1
             row_counter = 2
@@ -66,46 +79,47 @@ class Scraper():
             for index in range(num_items):
                 #click next page if 50th item on page is parsed
                 if row_counter % 50 == 0:
-                    print("MOD")
                     page_counter += 1
+                    print("NEXT PAGE")
+                    #reset the row counter
                     row_counter = 2
-                    
-                    #set up url for next page, (selecting 'next page' with xpath not working)
-                    next_page_url = self.driver.current_url.split("?")
-                    next_page_url.insert(1, f"?pg={page_counter}&")
-                    next_page_url = "".join(next_page_url)
-                    print(next_page_url)
-                    self.driver.get(next_page_url)
+                    #set url for the next page
+                    current_url = self.driver.current_url
+                    if "pg=" not in current_url: #page num = 1
+                        current_url = current_url.split("?")
+                        current_url.insert(1, f"?pg={page_counter}&")
+                        new_url = "".join(current_url)
+                    else:
+                        old_page_num = current_url[current_url.index("=")+1:current_url.index("&")]
+                        print("old_page_num", old_page_num)
+                        new_url = current_url.replace(str(old_page_num), str(int(old_page_num)+1))
 
+                    self.driver.get(new_url)
+                          
                 #select the item id from current row
-                try:
+                try: 
                     item_id = self.driver.find_element(By.XPATH, f'//*[@id="ItemEditForm"]/table[1]/tbody/tr/td/table/tbody/tr[{row_counter}]/td[2]/font/a[1]').text
                     print(item_id)
-                except:
-                    print("ID ERROR...")
-                
+                    self.item_ids.append(item_id)
+                except:pass
+
                 row_counter += 1
+            
+            self.store_as_json(self.paths[theme_path_index])
+            self.item_ids = []
                 
-            print("BEFORE BACK")
-            self.driver.get('https://www.bricklink.com/catalogTree.asp?itemType=S')
-            time.sleep(5)
 
-#https://www.bricklink.com/catalogList.asp?pg=1catType=S&catString=516 NO SETS
-#https://www.bricklink.com/catalogList.asp?pg=1&catType=S&catString=516 SETS
-
-
-
-    def store_as_json(self) -> None:
+    def store_as_json(self, theme_path) -> None:
+        
         theme_dicts = []
-        with open("theme_details.json", "w") as file:
-            for theme_path in self.paths:
-                theme_dicts.append({
-                    "path":theme_path,
-                    "items":{
-                        "minifigs":[],
-                        "sets":[],
-                    }
-                })
+        with open("theme_details.json", "a+") as file:
+            theme_dicts.append({
+                "path":theme_path,
+                "items":{
+                    "minifigs":[],
+                    "sets":self.item_ids,
+                }
+            })
             themes_json = json.dumps(theme_dicts, indent=4)
             file.write(themes_json)
 
@@ -113,6 +127,9 @@ class Scraper():
 #    themes = [theme for theme in THEMES.rsplit("\n") if theme != ''] 
 
 def main():
+    '''
+    cd OneDrive/Documents/programming/python/api's/bl_api/my_scripts
+    '''
     scraper = Scraper()
     scraper.setup()
     scraper.parse_themes(0)
