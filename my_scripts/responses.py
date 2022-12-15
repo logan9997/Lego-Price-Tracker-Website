19b315e9499d9835ab15f3935263d62cbc9017fb
logan9997 <80635281+logan9997@users.noreply.github.com>
2022-12-15 23:09:21 +0000

    some minor tweaks

--- my_scripts/responses.py
+++ my_scripts/responses.py
@@ -1,6 +1,6 @@
 from requests_oauthlib import OAuth1Session
 
-from my_scripts.KEYS_DONT_PUSH import *
+from my_scripts.keys import *
 
 import json
 
@@ -11,19 +11,19 @@ class Respose():
         self.auth = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, TOKEN_VALUE, TOKEN_SECRET)
 
 
-    def get_response_data(self, sub_url:str, **display:bool) -> None:
+    def get_response_data(self, sub_url:str, **display:bool) -> dict[str]:
         display = display.get("display", False)
         response = self.auth.get(self.base_url + sub_url)   
         #format response into dict
         self.response = json.loads(response._content.decode("utf-8"))
+
+        if display:
+            print(self.response)
+
         if "data" in self.response:
-            if display:
-                print(self.response["data"])
             return self.response["data"]
         else:
-            if display:
-                print(f'ERROR AT {sub_url}\n{self.response["meta"]}\n')
-            return None
+            return {"ERROR":self.response}
 
 
 
