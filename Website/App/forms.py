06d634495975bb0dd25c33985f7bb2d70272cd43
logan9997 <80635281+logan9997@users.noreply.github.com>
2022-12-22 23:50:20 +0000

    forms for login and sign upas well as portfolio items sort and add

--- Website/App/forms.py
+++ Website/App/forms.py
@@ -1,7 +1,31 @@
 from django import forms
 
-class MinifigSelect(forms.Form):
-    minifig_id = forms.CharField(max_length=10)
+class ItemSelect(forms.Form):
+    item_id = forms.CharField(max_length=20)
 
 
+class AddItemToPortfolio(forms.Form):
+    item_id = forms.CharField(max_length=20)
+    condition = forms.CharField(max_length=1)
+    quantity = forms.IntegerField()
 
+
+class PortfolioItemsSort(forms.Form):
+    sort_field = forms.MultipleChoiceField(choices=(
+        ("item_name", "item_name"), ("condition", "condition")
+    ))
+    field_order = forms.MultipleChoiceField(choices=(
+        ("ASC", "ASC"), ("DESC", "DESC")
+    ))
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=16)
+    password = forms.CharField(max_length=22)
+
+
+class SignupFrom(forms.Form):
+    email = forms.CharField(max_length=60)
+    username = forms.CharField(max_length=16)
+    password = forms.CharField(max_length=22)
+    password_confirmation = forms.CharField(max_length=22)
