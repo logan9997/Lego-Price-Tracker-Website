22b036e350e1f28b030a8101fd0c3aebb52d82e7
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-01-05 20:56:48 +0000

    fixed theme view, can accept string with multiple '/'

--- Website/App/urls.py
+++ Website/App/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('item/<str:item_id>/', views.item, name="item"),
     path('trending/', views.trending, name="trending"),
     path('search/', views.search, name="search"),
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('theme/<str:themes>', views.theme, name="theme"),
+    path('portfolio/<str:view>', views.portfolio, name="portfolio"),
+    path('theme/<path:theme_path>', views.theme, name="theme"),
     path('login/', views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
     path('join/', views.join, name="join"),
 ]
