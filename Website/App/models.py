23cd4d8b0eb1cc06ebc38e2c28db575a9356797a
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-01-22 15:17:09 +0000

    watchlist table added

--- Website/App/models.py
+++ Website/App/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+DATE_FORMAT = 'YYYY-MM-DD'
+DATE_DEFAULT = '1991-01-01'
+
 class Item(models.Model):
     item_id = models.CharField(max_length=20, primary_key=True)
     item_name = models.CharField(max_length=100)
@@ -12,7 +15,7 @@ class Item(models.Model):
 class Price(models.Model):
     price_record = models.AutoField(primary_key=True)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    date = models.DateField('%d-%m-%Y')
+    date = models.DateField(DATE_FORMAT, blank=True, null=True, default=DATE_DEFAULT)
     avg_price = models.FloatField()
     min_price = models.FloatField()
     max_price = models.FloatField()
@@ -22,14 +25,20 @@ class Price(models.Model):
 class Theme(models.Model):
     theme_id = models.AutoField(primary_key=True)
     theme_path = models.CharField(max_length=140)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE,blank=True, null=True)
 
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=16)
+    username = models.CharField(max_length=16, unique=True) #USERNAME MUST BE UNIQUE
     email = models.EmailField()
     password = models.CharField(max_length=22)
+    email_preference = models.CharField(max_length=14, default="All", choices=(
+        ("Never", "Never"), ("Occasional", "Occasional"),
+        ("All", "All")
+    ))
+    date_joined = models.DateField(DATE_FORMAT, blank=True, null=True, default=DATE_DEFAULT)
+    region = models.CharField(max_length=60, default='None')
 
 
 class Portfolio(models.Model):
@@ -40,4 +49,11 @@ class Portfolio(models.Model):
         ("U", "used"), ("N", "new")
     ))
     quantity = models.IntegerField()
-    #date_added = models.DateField('%d-%m-%Y',  null=True, default=None)
+    date_added = models.DateField(DATE_FORMAT, blank=True, null=True, default=DATE_DEFAULT)
+    view_count = models.IntegerField(default=0)
+
+
+class Watchlist(models.Model):
+    entry = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
