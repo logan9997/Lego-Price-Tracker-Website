9543d562cc38822d768cc84f5399fc6eb3dbd742
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-03-15 23:53:02 +0000

    validation for entry form working

--- Website/App/static/App/script.js
+++ Website/App/static/App/script.js
@@ -14,7 +14,7 @@ function display_item_delete_quantity() {
 }
 
 function logout_popup(e) {
-    if (confirm("Are you sure?")) {
+    if (confirm("Logging out? Are you sure?")) {
         window.location.reload()
     } else {
         e.preventDefault()
@@ -40,3 +40,118 @@ function reveal_sub_themes(theme_path) {
         theme_path.style.display = "none";  
     }
 }
+
+function update_add_portfolio_text() {
+    var button = document.getElementById("portfolio-button")
+    var value = document.getElementById("quantity-input").value
+
+    if (value < 0) {
+        button.innerHTML = "Remove from Portfolio!"
+        console.log("less")
+    } else {
+        button.innerHTML = "Add to Portfolio!"
+        console.log("more")
+    }
+}
+
+//https://www.scaler.com/topics/date-validation-in-javascript/
+function isValidDate(date) {
+ 
+    // Date format: YYYY-MM-DD
+    var datePattern = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+
+    // Check if the date string format is a match
+    var matchArray = date.match(datePattern);
+    if (matchArray == null) {
+        return false;
+    }
+
+    // Remove any non digit characters
+    var dateString = date.replace(/\D/g, ''); 
+
+    // Parse integer values from the date string
+    var year = parseInt(dateString.substr(0, 4));
+    var month = parseInt(dateString.substr(4, 2));
+    var day = parseInt(dateString.substr(6, 2));
+   
+    // Define the number of days per month
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // Leap years
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+        daysInMonth[1] = 29;
+    }
+
+    if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
+        return false;
+    }
+    return true;
+}
+
+
+function input_validation(input, type) {
+    var form = input.parentElement.parentElement.parentElement
+
+    var inputs = form.getElementsByTagName("input")
+    var new_inputs = []
+    for (let i = 0; i<inputs.length; i++) {
+        if (inputs[i].type != "hidden") {
+            new_inputs.push(inputs[i])
+        }
+    }
+    inputs = new_inputs
+    
+    var msg = ""
+    if (type == "date") {
+        if (! isValidDate(input.value) || input.value.length != 10) {
+            msg = "invalid date format. (YYYY-MM-DD)"
+        }
+    } else {
+        if (isNaN(input.value)) {
+            msg = "invalid number (0.00)"
+        } 
+        if (input.value < 0) {
+            msg = "number must be >= 0"
+        }
+    }
+
+    if (msg != "") {
+        form.parentElement.getElementsByClassName("error-msg-container")[0].style.display = "block";
+        form.parentElement.getElementsByClassName("error-msg")[0].innerHTML = msg;
+        form.parentElement.getElementsByClassName("entry-counter")[0].style["border-bottom"] = "none";
+    } else {
+        form.parentElement.getElementsByClassName("error-msg-container")[0].style.display = "none"
+        form.parentElement.getElementsByClassName("error-msg")[0].innerHTML = ""
+    }
+
+    var valid = true
+    for (let i = 0; i<inputs.length; i++) {
+        if (i % 2 == 0) {
+            if (! isValidDate(inputs[i].value) && inputs[i].value != "" && inputs[i].value.length != 10) {
+                valid = false
+                console.log(inputs[i].value, "IN-VALID")
+            }
+        } else {            
+            if (isNaN(inputs[i].value) || parseFloat(inputs[i].value) < 0) {
+                valid = false
+                console.log(inputs[i].value, "IN-VALID")
+            } 
+        }
+    }
+
+    for (let i = 0; i<inputs.length; i ++) {
+        console.log(inputs[i].value)
+    }
+
+    console.log("VALID", valid)
+
+    if (valid) {
+        console.log("form submitting...")
+        form.submit() 
+    } else {
+        console.log("form invalid...")
+        form.reset()
+    }
+}
+
+
